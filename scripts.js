52b189867aca8ea33d233bdd5d810aef885edf18
Abir Chowdhury <abirchowdhury0928@gmail.com>
2025-01-07 11:08:57 +0600

    Add show more button

--- scripts.js
+++ scripts.js
@@ -22,20 +22,22 @@ document
 //  Write your JavaScript code here...
 const searchResults = document.querySelector("#searchResults");
 const movieInfo = document.querySelector("#movieInfo");
-
 const showMovieDetails = (imdbID) => {
   movieInfo.innerHTML = "";
   fetch(`http://www.omdbapi.com/?apikey=e4727508&i=${imdbID}`)
     .then((response) => response.json())
     .then((movie) => {
-      console.log(movie.Actors);
+
+      const posterSrc = movie.Poster === "N/A"
+          ? "https://placehold.co/600x400/png"
+          : movie.Poster;
 
       const movieCard = `
             <div class="glass-effect bg-gray-300 rounded-xl p-8 transform transition-all duration-500">
                 <div class="grid md:grid-cols-3 gap-8">
                     <div class="md:col-span-1">
                         <!-- Poster Image -->
-                        <img src="${movie.Poster}"
+                        <img src="${posterSrc}"
                             alt="${movie.Title}"
                             class="w-full rounded-xl shadow-2xl hover:scale-105 transition-transform duration-300">
 
@@ -65,12 +67,11 @@ const showMovieDetails = (imdbID) => {
 
                         <!-- Movie Genre  -->
                         <div class="flex flex-wrap gap-2 mb-6">
-                        ${
-                          movie.Genre
-                            .split(",")
-                            .map((genre) => `<span class="px-3 py-1 rounded-full glass-effect text-sm bg-gray-400">${genre}</span>`)
-                            .join("")
-                        }
+                        ${movie.Genre
+          .split(",")
+          .map((genre) => `<span class="px-3 py-1 rounded-full glass-effect text-sm bg-gray-400">${genre}</span>`)
+          .join("")
+        }
                             
                         </div>
 
@@ -103,30 +104,39 @@ const showMovieDetails = (imdbID) => {
     });
 };
 
-function showMovieInfo(movie) {
-  fetch(`http://www.omdbapi.com/?apikey=e4727508&s=${movie}`)
+function showMovieInfo(movie, page = 1) {
+  fetch(`http://www.omdbapi.com/?apikey=e4727508&s=${movie}&page=${page}`)
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       data.Search.forEach((movie) => {
+        const posterSrc = movie.Poster === "N/A"
+          ? "https://placehold.co/600x400/png"
+          : movie.Poster;
         const movieCard = `
         <div class="movie-card glass-effect  rounded-xl overflow-hidden cursor-pointer hover:shadow-xl bg-gray-300" onclick="showMovieDetails('${movie.imdbID}')">
-            <div class="aspect-[2/3] bg-gray-300 dark:bg-gray-800 relative overflow-hidden">
-                <!-- Movie Poster -->
-                <img src="${movie.Poster}"
-                    alt="${movie.Title}"
-                    class="w-full h-full object-cover hover:scale-110 transition-transform duration-300">
-            </div>
-            <div class="p-4">
-                <!-- Movie Title -->
-                <h3 class="font-semibold text-gray-900 dark:text-white line-clamp-1">${movie.Title}</h3>
-                <!-- Movie Year -->
-                <p class="text-sm text-gray-600 dark:text-gray-400">${movie.Year}</p>
-            </div>
-        </div>`;
+        <div class="aspect-[2/3] bg-gray-300 dark:bg-gray-800 relative overflow-hidden">
+            <!-- Movie Poster -->
+            <img src="${posterSrc}"
+                alt="${movie.Title}"
+                class="w-full h-full object-cover hover:scale-110 transition-transform duration-300">
+        </div>
+        <div class="p-4">
+            <!-- Movie Title -->
+            <h3 class="font-semibold text-gray-900 dark:text-white line-clamp-1">${movie.Title}</h3>
+            <!-- Movie Year -->
+            <p class="text-sm text-gray-600 dark:text-gray-400">${movie.Year}</p>
+        </div>
+    </div>`;
 
         searchResults.innerHTML += movieCard;
       });
     });
+}
+let conter = 1
+
+function home() {
+  window.location.reload();
 }
 
 document.querySelector("#searchInput").addEventListener("keypress", (e) => {
@@ -134,9 +144,22 @@ document.querySelector("#searchInput").addEventListener("keypress", (e) => {
     if (e.target.value != "") {
       searchResults.innerHTML = "";
       movieInfo.innerHTML = "";
-      showMovieInfo(e.target.value);
+      conter = 1
+      showMovieInfo(e.target.value, conter);
     } else {
       window.location.reload();
     }
+  }
+});
+
+document.querySelector("#searchButton").addEventListener("click", (e) => {
+  console.log("clicked");
+  const searchInput = document.querySelector("#searchInput");
+  if (searchInput.value != "") {
+    movieInfo.innerHTML = "";
+    conter += 1
+    showMovieInfo(searchInput.value, conter);
+  } else {
+    window.location.reload();
   }
 });
